3fd4a9e7ba44a0148839a7ab329276885b5fd6c1
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-08-23 18:59:11 +0200

    chore(main): launch with configuration from config file

--- aot/main.py
+++ aot/main.py
@@ -2,6 +2,7 @@ import asyncio
 from aiohttp import web
 from autobahn.asyncio.websocket import WebSocketServerFactory
 
+import aot
 from aot.api import Api
 from aot.api import get_board
 
@@ -9,14 +10,18 @@ from aot.api import get_board
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
-    factory = WebSocketServerFactory("ws://localhost:9000", debug=True)
+    host = aot.config['api']['host']
+    ws_endpoint = 'ws://{host}:{port}'.format(
+        host=host,
+        port=aot.config['api']['ws_port'])
+    factory = WebSocketServerFactory(ws_endpoint, debug=True)
     factory.protocol = Api
-    server = loop.create_server(factory, '127.0.0.1', 9000)
+    server = loop.create_server(factory, host, aot.config['api']['ws_port'])
     wsserver = loop.run_until_complete(server)
 
     webapp = web.Application(loop=loop)
     webapp.router.add_route('GET', '/board/{name}', get_board)
-    server = loop.create_server(webapp.make_handler(), '127.0.0.1', 9090)
+    server = loop.create_server(webapp.make_handler(), host, aot.config['api']['http_port'])
     httpserver = loop.run_until_complete(server)
 
     try:
