5073139182692c8b725a3ef8cd81b34fb45fb30d
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-08-26 22:38:01 +0200

    test(api): add test using websockets and coroutine and one using Autobahn

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name='aot',
     version='0.1',
     packages=find_packages(),
-    install_requires=['aiohttp', 'autobahn', 'lxml', 'redis', 'toml', 'websocket-client'],
+    install_requires=['aiohttp', 'autobahn', 'lxml', 'redis', 'toml', 'websockets'],
     tests_require=['pytest', 'pytest-asyncio', 'pytest-cov'],
     cmdclass={'test': PyTest},
     author='jenselme',
