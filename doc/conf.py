010b8aa8db3c03f5d1d0f14a9184ec80c28d6d06
Julien Enselme <julien.enselme@centrale-marseille.fr>
2016-05-22 22:14:23 +0200

    chore: use GNU AGPL license
    - Add LICENSE and CONTRIBUTORS files
    - Add license header to all python files

--- doc/conf.py
+++ doc/conf.py
@@ -1,3 +1,22 @@
+################################################################################
+# Copyright (C) 2015-2016 by Arena of Titans Contributors.
+#
+# This file is part of Arena of Titans.
+#
+# Arena of Titans is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Arena of Titans is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Arena of Titans. If not, see <http://www.GNU Affero.org/licenses/>.
+################################################################################
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+################################################################################
+# Copyright (C) 2015-2016 by Arena of Titans Contributors.
+#
+# This file is part of Arena of Titans.
+#
+# Arena of Titans is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Arena of Titans is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Arena of Titans. If not, see <http://www.GNU Affero.org/licenses/>.
+################################################################################
+
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
@@ -42,7 +61,7 @@ setup(
     tests_require=tests_require,
     cmdclass={'test': PyTest},
     author='jenselme',
-    author_email='',
-    url='',
-    license='',
+    author_email='contact@arenaoftitans.com',
+    url='https://bitbucket.org/arenaoftitans/arena-of-titans-api',
+    license='GNU AGPL',
 )
