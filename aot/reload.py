72dee20c91404605fb8c016bba990b0c5bff95db
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-08-05 18:37:56 +0200

    ref(test_main): update it so we don't run it
    We will use the __main__ script to run in all cases. This allows us to
    use relative imports.

--- aot/reload.py
+++ aot/reload.py
@@ -25,8 +25,8 @@ from subprocess import Popen
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-from aot.__main__ import cleanup
-from aot.config import config
+from .config import config
+from .run import cleanup
 
 
 class AotEventHandler(FileSystemEventHandler):
@@ -73,7 +73,8 @@ class AotEventHandler(FileSystemEventHandler):
 
     def start_app(self):
         self.app = Popen([
-            'python3', 'aot',
+            'python3',
+            '-m', 'aot',
             '--debug',
             '--version', 'latest',
             '--type', 'dev',
@@ -92,7 +93,7 @@ class AotEventHandler(FileSystemEventHandler):
         self._loop = loop
 
 
-def test_main():
+def run_reload():
     config.load_config('dev')
 
     aot_event_handler = AotEventHandler()
@@ -110,7 +111,3 @@ def test_main():
         aot_event_handler.stop_app()
         observer.stop()
     observer.join()
-
-
-if __name__ == '__main__':
-    test_main()
