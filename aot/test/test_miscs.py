e6aa2f163ae90f96cb974d85e7450d86723f1b7d
Julien Enselme <jenselme@jujens.eu>
2018-03-08 10:52:51 +0100

    test(miscs): make sure the sign key is never defined
    If it is defined in the environment, the app starts and the test is
    blocked. We also add a timeout so the test fails if the app were to
    start.

--- aot/test/test_miscs.py
+++ aot/test/test_miscs.py
@@ -37,9 +37,11 @@ def test_get_cards_list():
             assert action.args['distance'] == 1
 
 
+@pytest.mark.timeout(1)
 def test_no_startup_not_dev_no_cache_sign_key(monkeypatch):
     # Use a fake ENV, since it must raise for anything that is not development.
     monkeypatch.setenv('ENV', 'mock')
+    monkeypatch.setenv('CACHE_SIGN_KEY', '')
 
     with pytest.raises(EnvironmentError):
         main()
