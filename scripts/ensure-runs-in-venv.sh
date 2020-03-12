e24ff191a42cb9afed72b347d18687cfac8d12d7
Julien Enselme <jenselme@jujens.eu>
2020-03-12 10:54:57 +0000

    fix(bin): make sure pre-commit won't fail if bashrc contains unbound variables

--- scripts/ensure-runs-in-venv.sh
+++ scripts/ensure-runs-in-venv.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 command="$1"
 # To be sure $@ will only contain the parameters of the command, not the command itself.
 shift
@@ -12,7 +14,10 @@ if [[ -v VIRTUAL_ENV ]]; then
     exit $?
 else
     # Load bashrc to be sure PATH is correctly set. We can't do this before this it could mess up with the enabled venv.
+    # We don't want to fail if the bashrc file contains unbound variables.
+    set +u
     source ~/.bashrc || echo "Bash RC file not found"
+    set -u
     # If not and pipenv is installed, run with pipenv.
     if command -v pipenv > /dev/null; then
         echo "Running in venv with pipenv"
